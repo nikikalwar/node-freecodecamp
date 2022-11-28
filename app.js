660fb58b87eae6fbde87c4a924923abedbaa9cfd
Niki.Kalwar <niki@credibled.com>
2022-11-28 13:00:36 +0530

    matching and removing the brackets

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
-//so what are modules
 
-//modules are objects which are used by node js to share libraries or code
+const balancer=(string)=>{
+    let str=string;
+    if(str.length===0) return console.log("empty params passed")
+    let popArr=str.split("")
+    if(str.length%2!==0) return console.log("Not Balanced");
+    else{
+        let strArr=string.split("");
+        let length=strArr.length;
+        let reverseString=string.split("");
+        reverseString=reverseString.reverse();
+        for(let i=0;i<(length/2);i++){
+            if(strArr[i]==='{' && reverseString[i]==='}'){
+                popArr.shift();
+                popArr.pop();
+            }
+            if(strArr[i]==='[' && reverseString[i]===']'){
+                popArr.shift();
+                popArr.pop();
+            }
+            if(strArr[i]==='(' && reverseString[i]===')'){
+                popArr.shift();
+                popArr.pop();
+            }
+        }
+        if(popArr.length>0) return "NO";
+        else return "YES"
+}}
 
-//local
-const dontShare="dont export this values";
-
-//sharing
-const userName="Niki";
-const adminName="John";
-
-module.exports={userName,adminName}
+let result=balancer('{[()]}');
+console.log(result)
+result=balancer('{[(])}');
+console.log(result)
+result=balancer('{{[[(())]]}}')
+console.log(result)
